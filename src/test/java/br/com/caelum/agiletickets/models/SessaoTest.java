e2299a6346c1b5ace418d24dc561e17af1106ff3
Ortegal <ricardo.ortegal@gmail.com>
2014-06-04 20:31:31 -0300

    correcao do bug

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,14 +6,6 @@ import org.junit.Test;
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
-	}
-	
-	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
@@ -36,6 +28,13 @@ public class SessaoTest {
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+	}
+	
+	@Test
+	public void deveVender1IngressoSeHa1Vaga(){
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
 	
 }
